893d7aa9dd86608a31d1fc7b4e92fbe24d0832fc
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2025-09-17 12:05:04 -0400

    fix: improve currency conversion logic

--- Lab 3 Currency Converter UI/script.js
+++ Lab 3 Currency Converter UI/script.js
@@ -3,7 +3,19 @@ const secondCurrencyField = document.getElementById("currency-two");
 const convertButton = document.querySelector("button");
 
 convertButton.addEventListener("click", () => {
-    let currencyOneValue = parseInt(firstCurrencyField.value);
-    let currencyTwoValue = parseInt(secondCurrencyField.value);
-    secondCurrencyField.value = currencyOneValue * 62.20;
+    let currencyOneValue = firstCurrencyField.value;
+    let currencyTwoValue = secondCurrencyField.value;
+    console.log(currencyOneValue, currencyTwoValue);
+    if (currencyTwoValue == "" && currencyOneValue == "") {
+        alert("Insert a currency in at least one text field.");
+    }
+    else if (!(currencyOneValue == "" && currencyTwoValue == "")) {
+        alert("Empty at least one of the text fields.");
+    }
+    else if (currencyOneValue == "") {
+        firstCurrencyField.value = (parseInt(currencyTwoValue) * 0.016).toFixed(2);
+    }
+    else if (currencyTwoValue == "") {
+        secondCurrencyField.value = (parseInt(currencyOneValue) * 62.20).toFixed(2);
+    }
 })
