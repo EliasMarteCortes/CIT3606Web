49cef6e80ceced1c4383313f192b9100bc9fe3da
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2025-09-24 13:27:09 -0400

    feat: add event listener for size selection to adjust landmark image dimensions

--- Lab 4 Landmark Tour/script.js
+++ Lab 4 Landmark Tour/script.js
@@ -1,5 +1,6 @@
 const landmark = document.querySelector("#landmarkimg");
 const radiobtns = document.querySelector("#radioContainer");
+const menu = document.querySelector("#sizes");
 
 radiobtns.addEventListener("click", (e) => {
     switch(e.target.id) {
@@ -11,6 +12,23 @@ radiobtns.addEventListener("click", (e) => {
             break;
         case "stonehenge":
             landmark.src = "stonehenge.jpg";
+            break;
+    }
+})
+
+menu.addEventListener("change", () => {
+    switch (menu.value) {
+        case "small":
+            landmark.style.height = "100px";
+            landmark.style.width = "180px";
+            break;
+        case "medium":
+            landmark.style.height = "200px";
+            landmark.style.width = "280px";
+            break;
+        case "large":
+            landmark.style.height = "300px";
+            landmark.style.width = "380px";
             break;
     }
 })
