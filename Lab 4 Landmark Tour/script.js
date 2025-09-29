873421370de4a860dfb0a2c8124f3b5a3d951c10
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2025-09-29 11:23:41 -0400

    feat: add descriptions for landmarks in the image selection

--- Lab 4 Landmark Tour/script.js
+++ Lab 4 Landmark Tour/script.js
@@ -1,17 +1,21 @@
 const landmark = document.querySelector("#landmarkimg");
 const radiobtns = document.querySelector("#radioContainer");
 const menu = document.querySelector("#sizes");
+const descDiv = document.querySelector("#imgDesc");
 
 radiobtns.addEventListener("click", (e) => {
     switch(e.target.id) {
         case "handaragate":
             landmark.src = "handaragate.jpg";
+            descDiv.textContent = "The Handara Gate is in Bali, Indonesia and is a traditional Balinese temple gateway surrounded by mountains and gardens."
             break;
         case "mtfuji":
             landmark.src = "mtfuji.jpg";
+            descDiv.textContent = "Mt. Fuji is in Japan and is a 3,776-meter volcano with a snow-capped peak."
             break;
         case "stonehenge":
             landmark.src = "stonehenge.jpg";
+            descDiv.textContent = "Stonehenge is in Wiltshire, England and is a 5,000-year-old prehistoric stone circle."
             break;
     }
 })
