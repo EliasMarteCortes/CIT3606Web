b489d7d7a671a213a5d701a17d88f9f8b75d49f1
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2025-10-14 19:39:40 -0400

    feat: draw star image in canvas

--- Lab 5 Canvas Game/script.js
+++ Lab 5 Canvas Game/script.js
@@ -2,6 +2,10 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
 let score = 0;
+let starX = 100;
+let starY = 100;
+let starTargetX = 400;
+let starTargetY = 300;
 
 const starImg = new Image();
 starImg.src = 'star.png';
@@ -10,8 +14,11 @@ const voidImg = new Image();
 voidImg.src = 'void.png';
 
 function drawFunction() {
-            ctx.fillStyle = '#0d0d1e';
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = '#0d0d1e';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(starImg, starX - 40, starY - 40, 80, 80);
+
+    requestAnimationFrame(drawFunction);
 }
 
 drawFunction();
