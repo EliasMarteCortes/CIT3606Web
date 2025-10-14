3b94c58f104aa1504c3ecd36a7752699e2901252
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2025-10-14 19:26:53 -0400

    feat: implement canvas setup and initial drawing function

--- Lab 5 Canvas Game/script.js
+++ Lab 5 Canvas Game/script.js
@@ -1,2 +1,17 @@
-const canvas = document.querySelector('#gameCanvas');
+const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
+
+let score = 0;
+
+const starImg = new Image();
+starImg.src = 'star.png';
+
+const voidImg = new Image();
+voidImg.src = 'void.png';
+
+function drawFunction() {
+            ctx.fillStyle = '#0d0d1e';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+drawFunction();
