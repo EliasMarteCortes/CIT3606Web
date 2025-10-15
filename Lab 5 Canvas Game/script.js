8929085d62c2bd0220a09022b36315873aa76e14
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2025-10-14 20:42:46 -0400

    feat: add chance of void spawning

--- Lab 5 Canvas Game/script.js
+++ Lab 5 Canvas Game/script.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
+const scoreDisplay = document.getElementById('score');
 
 let score = 0;
 stars = [];
@@ -11,11 +12,14 @@ const voidImg = new Image();
 voidImg.src = 'void.png';
 
 function createStar() {
+    const isVoid = Math.random() < 0.2;
+
     const star = {
         x: Math.random() * 620 + 40,
         y: Math.random() * 470 + 40,
         targetX: Math.random() * 620 + 40,
         targetY: Math.random() * 470 + 40,
+        isVoid: isVoid,
     };
     stars.push(star);
 }
@@ -24,7 +28,11 @@ function drawFunction() {
     ctx.fillStyle = '#0d0d1e';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < stars.length; i++) {
-        ctx.drawImage(starImg, stars[i].x - 40, stars[i].y - 40, 80, 80);
+        if (stars[i].isVoid) {
+            ctx.drawImage(voidImg, stars[i].x - 40, stars[i].y - 40, 80, 80);
+        } else {
+            ctx.drawImage(starImg, stars[i].x - 40, stars[i].y - 40, 80, 80);
+        }
     }
     
     requestAnimationFrame(drawFunction);
@@ -57,8 +65,19 @@ canvas.addEventListener('mousedown', function(e) {
         const dist = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
         
         if (dist < 40) {
-            stars.splice(i, 1);
-            alert('You caught a Star!');
+            if (stars[i].isVoid) {
+                alert('Oh no! You touched a Dark Void!\n-5 Points!');
+                score -= 5;
+                scoreDisplay.textContent = score;
+                stars.splice(i, 1);
+                return;
+            }
+            else {
+                stars.splice(i, 1);
+                alert('You caught a Star!\n+10 Points!');
+                score += 10;
+                scoreDisplay.textContent = score;
+            }
             break;
         }
     }
