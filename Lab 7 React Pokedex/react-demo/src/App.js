b02e6e3103d4f70a04a0964f3690afba98aa21c6
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2025-10-26 18:28:19 -0400

    feat: add pokemon types to pokemon card function

--- Lab 7 React Pokedex/react-demo/src/App.js
+++ Lab 7 React Pokedex/react-demo/src/App.js
@@ -5,6 +5,7 @@ function Pokemon(props) {
     <div className="pokemon-card">
       <h2>{props.name}</h2>
       <img src={props.image}/>
+      <p>{props.type}</p>
     </div>
   );
 }
@@ -14,9 +15,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Pokedex</h1>
-        <Pokemon name="Zangoose" image="images\zangoose.png"/>
-        <Pokemon name="Mudkip" image="images\mudkip.png"/>
-        <Pokemon name="Metapod" image="images\metapod.png"/>
+        <Pokemon name="Zangoose" image="images\zangoose.png" type="Normal"/>
+        <Pokemon name="Mudkip" image="images\mudkip.png" type="Water"/>
+        <Pokemon name="Metapod" image="images\metapod.png" type="Bug"/>
       </header>
     </div>
   );
