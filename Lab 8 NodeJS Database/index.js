1197b1dc5f3ef7c668850e0c35d294d6bf446b4e
Elias Illinois Marte Cortes <eliascortesmartes@gmail.com>
2025-11-16 21:43:29 -0500

    feat: implement database connection and query for student records

--- Lab 8 NodeJS Database/index.js
+++ Lab 8 NodeJS Database/index.js
@@ -1,6 +1,26 @@
+const mysql = require('mysql');
+require('dotenv').config();               
+const conn = mysql.createConnection({
+  host: "mysql1-p2.ezhostingserver.com",
+  database: "citdemo",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+});
+
+
 const express = require('express'); 
 const app = express();
 app.get('/', function(req, res){
-   res.send("Hello world!");
+   conn.connect((err) => {
+  const sql = 'SELECT * FROM students WHERE lastname = ?';
+const lastname = "Mouse";
+conn.query(sql, [lastname], function (err, result) {
+  if (err) throw err;
+  console.log(result);
+ res.send(result);
+});
+
+
+});
 });
 app.listen(8080);
